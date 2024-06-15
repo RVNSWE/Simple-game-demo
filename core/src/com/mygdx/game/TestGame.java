2c5ba2698404a96fe180da92fb86ee35d4212273
Cyonica <monkey_lady@hotmail.com>
2024-06-15 16:01:33 +0100

    balls!

---
+++ core/src/com/mygdx/game/TestGame.java
@@ -0,0 +1,40 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.GL20;
+import java.util.ArrayList;
+import java.util.Random;
+
+// https://colourtann.github.io/HelloLibgdx/2.html
+
+public class TestGame extends ApplicationAdapter {
+	ShapeRenderer shape;
+	ArrayList<Ball> balls = new ArrayList<>();
+	Random r = new Random();
+
+	@Override
+	public void create () {
+		shape = new ShapeRenderer();
+		for (int i = 0; i < 10; i++) {
+			balls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()),
+					r.nextInt(Gdx.graphics.getHeight()),
+					r.nextInt(100),
+					r.nextInt(15),
+					r.nextInt(15)));
+		}
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+		for (Ball ball : balls) {
+			ball.update();
+			ball.draw(shape);
+		}
+		shape.end();
+	}
+}
